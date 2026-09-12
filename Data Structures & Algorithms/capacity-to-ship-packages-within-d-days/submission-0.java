cc5a0216e58f4ab069ab0d34ebebcd6c614dc75a
SANTOSH777-ai <ssaisantosheluri@gmail.com>
2026-09-12 15:33:17 +0530

    Add: capacity-to-ship-packages-within-d-days - submission-0

---
+++ Data Structures & Algorithms/capacity-to-ship-packages-within-d-days/submission-0.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int shipWithinDays(int[] weights, int days) {
+        int low = 0;
+        int high = 0;
+        for(int weight : weights){
+            low = Math.max(weight,low);
+            high += weight;
+        }
+        int answer = high;
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            int daysNeeded = 1;
+            int currentWeight = 0;
+            for(int weight : weights){
+                if(currentWeight + weight > mid){
+                    daysNeeded++;
+                    currentWeight = weight;
+                }else{
+                    currentWeight += weight;
+                }
+            }
+            if(daysNeeded <= days){
+                answer = mid;
+                high = mid - 1;
+            }else{
+                low = mid+1;
+            }
+        }
+        return answer;
+    }
+}
