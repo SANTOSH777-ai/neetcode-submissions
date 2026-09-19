da6092943c88aa8dbf0e5f13a9571cccf55735f1
SANTOSH777-ai <ssaisantosheluri@gmail.com>
2026-09-19 13:42:42 +0530

    Add: move-zeroes - submission-0

---
+++ Data Structures & Algorithms/move-zeroes/submission-0.java
@@ -0,0 +1,12 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int s = 0;
+        for(int f = 0;f<nums.length;f++){
+            if(nums[f] == 0) continue;
+            int temp = nums[s];
+            nums[s] = nums[f];
+            nums[f] = temp;
+            s++;
+        }
+    }
+}
